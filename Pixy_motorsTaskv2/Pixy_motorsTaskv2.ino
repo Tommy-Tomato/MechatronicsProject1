17dcc7efaa1b0cde581823f9b168486ac30a8087
Tommy-Tomato <156547507+Tommy-Tomato@users.noreply.github.com>
2026-02-18 16:34:52 -0500

    Create Pixy_motorsTaskv2.ino

---
+++ Pixy_motorsTaskv2/Pixy_motorsTaskv2.ino
@@ -0,0 +1,190 @@
+#include <DualMAX14870MotorShield.h>
+#include <Pixy2.h>
+
+DualMAX14870MotorShield Motors;
+Pixy2 pixy;
+
+/*
+Motor1: LEFT
+Motor2: RIGHT
+*/
+
+// Encoder pins (consistent naming)
+const int Motor1EPinA = 2;
+const int Motor1EPinB = 3;
+
+const int Motor2EPinA = 18;
+const int Motor2EPinB = 19;
+
+volatile long counter1 = 0;
+volatile long counter2 = 0;
+
+// Motion constants
+float COUNTS_PER_REV = 12.0 * 4.0 * 20.0;
+float TURN_CIRC = 100;       // MEASURE THIS //180 for now
+float WHEEL_CIRC_CM = 16;    // MEASURE THIS
+float FULL_ROTATION_COUNTS = (TURN_CIRC / WHEEL_CIRC_CM) * COUNTS_PER_REV;
+
+int TURN_SPEED = 50;
+int STRAIGHT_SPEED = 100;
+
+enum States
+{
+  initialize,
+  main,
+  left,
+  right,
+  back
+};
+
+States myState = initialize;
+
+void setup()
+{
+  Motors.enableDrivers();
+
+  Serial.begin(9600);
+  pixy.init();
+  Serial.println("Pixy Initialized...");
+
+  pinMode(Motor1EPinA, INPUT_PULLUP);
+  pinMode(Motor1EPinB, INPUT_PULLUP);
+  pinMode(Motor2EPinA, INPUT_PULLUP);
+  pinMode(Motor2EPinB, INPUT_PULLUP);
+
+  attachInterrupt(digitalPinToInterrupt(Motor1EPinA), changeUp1A, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(Motor1EPinB), changeUp1B, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(Motor2EPinA), changeUp2A, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(Motor2EPinB), changeUp2B, CHANGE);
+}
+
+void loop()
+{
+  pixy.ccc.getBlocks();
+
+  switch (myState)
+  {
+    case initialize:
+      myState = main;
+      break;
+
+    case main:
+      Motors.setSpeeds(STRAIGHT_SPEED, STRAIGHT_SPEED);
+      Serial.println("spinning...");
+      if (pixy.ccc.numBlocks)
+      {
+        for (int i = 0; i < pixy.ccc.numBlocks; i++)
+        {
+          int sig = pixy.ccc.blocks[i].m_signature;
+
+          Serial.print("Detected Signature: ");
+          Serial.println(sig);
+
+          if (sig == 1)
+          {
+            Serial.println("Green - 180 Turn");
+            myState = back;
+            break;
+          }
+          else if (sig == 2)
+          {
+            Serial.println("Blue - Turn Right");
+            myState = right;
+            break;
+          }
+          else if (sig == 3)
+          {
+            Serial.println("Red - Turn Left");
+            myState = left;
+            break;
+          }
+        }
+      }
+      break;
+
+    case left:
+      Serial.println("turning left");
+      counter1 = 0;
+      counter2 = 0;
+
+      while (abs(counter1) < 0.5 * FULL_ROTATION_COUNTS)
+      {
+        Motors.setSpeeds(-TURN_SPEED, TURN_SPEED);
+        Serial.println(counter1);
+      }
+
+      Motors.setSpeeds(0, 0);   // STOP
+      myState = main;
+      break;
+
+    case right:
+      Serial.println("turning right");
+      counter1 = 0;
+      counter2 = 0;
+
+      while (abs(counter1) < 0.5 * FULL_ROTATION_COUNTS)
+      {
+        Motors.setSpeeds(TURN_SPEED, -TURN_SPEED);
+        Serial.println(counter1);
+      }
+
+      Motors.setSpeeds(0, 0);   // STOP
+      myState = main;
+      break;
+
+    case back:
+      Serial.println("turning around");
+      counter1 = 0;
+      counter2 = 0;
+
+      while (abs(counter1) < FULL_ROTATION_COUNTS)
+      {
+        Motors.setSpeeds(TURN_SPEED, -TURN_SPEED);
+        Serial.println(counter1);
+      }
+
+      Motors.setSpeeds(0, 0);   // STOP
+      myState = main;
+      break;
+
+    default:
+      Serial.println("Error!");
+      break;
+  }
+}
+
+// ======================
+// INTERRUPT ROUTINES
+// ======================
+
+void changeUp1A()
+{
+  if (digitalRead(Motor1EPinA) == digitalRead(Motor1EPinB))
+    counter1++;
+  else
+    counter1--;
+}
+
+void changeUp1B()
+{
+  if (digitalRead(Motor1EPinA) == digitalRead(Motor1EPinB))
+    counter1--;
+  else
+    counter1++;
+}
+
+void changeUp2A()
+{
+  if (digitalRead(Motor2EPinA) == digitalRead(Motor2EPinB))
+    counter2++;
+  else
+    counter2--;
+}
+
+void changeUp2B()
+{
+  if (digitalRead(Motor2EPinA) == digitalRead(Motor2EPinB))
+    counter2--;
+  else
+    counter2++;
+}
