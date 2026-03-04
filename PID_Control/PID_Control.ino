e5066c5b9c24f6a22e30c716919bdf737423d28a
Travolta Chiposi-Mudiwa <travoltamudiwa@gmail.com>
2026-03-04 16:20:46 -0500

    Update PID_Control.ino

--- PID_Control/PID_Control.ino
+++ PID_Control/PID_Control.ino
@@ -25,7 +25,25 @@ unsigned long lastTime = 0;
 void setup() {
   Serial.begin(9600);
   
+<<<<<<< Updated upstream
   Motors.enableDrivers();
+=======
+//IMU configs
+ //Initialize I2C communication
+  Wire.begin();
+
+  //Initialization of the BNO055
+  BNO_Init(&myBNO); //Assigning the structure to hold information about the device
+
+  //Configuration to NDoF mode
+  bno055_set_operation_mode(OPERATION_MODE_NDOF);
+
+  delay(1);
+//
+
+
+
+>>>>>>> Stashed changes
 }
 
 void loop() {
@@ -60,4 +78,46 @@ void applyOutput(double output) {
   Motors.setSpeeds(outputSpeed, -outputSpeed);
   
 }
+<<<<<<< Updated upstream
  
+=======
+
+
+
+#include "BNO055_support.h"		//Contains the bridge code between the API and Arduino
+#include <Wire.h>
+
+struct bno055_t myBNO;
+struct bno055_euler myEulerData; //Structure to hold the Euler data
+
+unsigned long lastTime = 0;
+
+
+void myIMU() 
+{
+  // Changed from 100ms to 20ms for a 50Hz refresh rate
+  if ((millis() - lastTime) >= 20) 
+  {
+    lastTime = millis();
+
+    // Update Euler data into the structure
+    bno055_read_euler_hrp(&myEulerData);			
+
+    // Convert raw data to degrees (Divide by 16.0 as per Bosch API)
+    float yaw   = (float)myEulerData.h / 16.00;
+    float pitch  = (float)myEulerData.r / 16.00;
+    float roll = (float)myEulerData.p / 16.00;
+
+    // Print values in a single line for easier reading in Serial Monitor
+    Serial.print("(Yaw(Z-axis)): "); Serial.print(yaw);
+    Serial.print(" Roll(Y-axis): "); Serial.print(roll);
+    Serial.print(" Pitch(X-axis): "); Serial.println(pitch);
+    Serial.println();
+
+  }
+  
+}
+
+
+
+>>>>>>> Stashed changes
